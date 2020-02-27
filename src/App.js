fce3f80fdf9a0631cd47b3c44ac1ed3831e346cf
michelle-hey <miscmiche@gmail.com>
2020-02-26 21:38:21 -0500

    MVP met and stretch for styling/quarter button functionality completed.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,87 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+
+
+  const touchDownHome = (e) => {
+    setHomeScore(homeScore + 7);
+  }
+  const fieldGoalHome = (e) => {
+    setHomeScore(homeScore + 3);
+  }
+
+  const touchDownAway = (e) => {
+    setAwayScore(awayScore + 7);
+  }
+  const fieldGoalAway = (e) => {
+    setAwayScore(awayScore + 3);
+  }
+
+  const reset = (e) => {
+    setHomeScore(0);
+    setAwayScore(0);
+    setQuarter(0);
+  };
+  
+  const leader = homeScore > awayScore ? `Home Team Wins` : `Away Team Wins`;
+
+  const nextQuarter = (e) => {
+    if(homeScore === awayScore && quarter === 4) {
+        setQuarter('OT');
+        console.log('going to OT');
+    } else if(quarter === 4){
+        alert(`The game is over! ${leader} wins!`); 
+        reset();
+    } else if(quarter === "OT"){
+        setQuarter(quarter + '2');
+    } else if(quarter === "OT2"){
+        if(homeScore === awayScore){
+          alert("Game is over in a Tie.");
+          reset();
+        } else {
+          alert(`The winner is ${leader}`);
+          reset();
+        }
+    } else setQuarter(quarter + 1);
+  };
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+            <h2 className="home__name">Cats</h2>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">Dogs</h2>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+
+        <BottomRow quarter={quarter} />
+
       </section>
       <section className="buttons">
+        <button onClick={nextQuarter}>Next Quarter</button>
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={touchDownHome} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={fieldGoalHome} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={touchDownAway} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={fieldGoalAway} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
